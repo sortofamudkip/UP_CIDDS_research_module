89d368d39dd01aefd7b3766ea8d5b5ef61895ff4
Sortofamudkip <wishyutp0328@gmail.com>
2023-02-25 23:47:01 +0100

    added load_data

---
+++ load_data.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EXTERNAL_DATASET_DIR_NAME = os.getenv("EXTERNAL_DATASET_DIR_NAME")
+
+
+def load_data_raw() -> pd.DataFrame:
+    """Loads data with no preprocessing.
+
+    Returns:
+        pd.DataFrame: the dataset.
+    """
+    dataset = pd.concat(
+        [
+            pd.read_csv(
+                f"{EXTERNAL_DATASET_DIR_NAME}CIDDS-001-external-week{i}.csv",
+                dtype={
+                    "Date first seen": str,
+                    "Duration": float,
+                    "Proto": "string",
+                    "Src IP Addr": "string",
+                    "Src Pt": int,
+                    "Dst IP Addr": "string",
+                    "Dst Pt": float,
+                    "Packets": int,
+                    "Bytes": "string",
+                    "Flows": int,
+                    "Flags": "string",
+                    "Tos": int,
+                    "class": "string",
+                    "attackType": "string",
+                    "attackID": "string",
+                    "attackDescription": "string",
+                },
+            )
+            for i in range(1, 4 + 1)
+        ]
+    )
+    return dataset
+
+
+def drop_unnecessary_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """Drops unnecessary columns from dataset.
+
+    Args:
+        df (pd.DataFrame): original dataset.
+
+    Returns:
+        pd.DataFrame: new dataset.
+    """
+    return df.drop(
+        ["Flows", "Tos", "attackType", "attackID", "attackDescription"], axis=1
+    )
+
+
+def load_data():
+    return drop_unnecessary_columns(load_data_raw())
+
+
+if __name__ == "__main__":
+    data = load_data()
+    print(len(data))
