3d1f84557dd31a69775fc566c98a3ea248338c5d
Sortofamudkip <wishyutp0328@gmail.com>
2023-02-27 02:48:42 +0100

    added test for _hex_string_to_TCP_flags

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,4 +1,4 @@
-import sys
+import sys, pytest
 
 sys.path.append("../main/")  # Adds higher directory to python modules path.
 
@@ -8,3 +8,14 @@ import load_data
 def test_load_data_len():
     data = load_data.load_data()
     assert len(data) == 671241
+
+
+@pytest.mark.parametrize(
+    "hex_str, TCP_string_result",
+    [
+        ("0xde", ".APRS."),
+        ("0xd2", ".A..S."),
+    ],
+)
+def test_hex_string_to_TCP_flags(hex_str, TCP_string_result):
+    assert load_data._hex_string_to_TCP_flags(hex_str) == TCP_string_result
