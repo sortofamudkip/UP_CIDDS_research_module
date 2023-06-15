85387cd72f89d8479e18f512cafb40a8ce3de2ec
Sortofamudkip <wishyutp0328@gmail.com>
2023-06-15 23:20:26 +0200

    optimise binary_stats

--- main/score_model.py
+++ main/score_model.py
@@ -4,24 +4,21 @@ from sklearn.metrics import (
     f1_score,
     precision_score,
     recall_score,
+    precision_recall_fscore_support,
 )
 from pandas import DataFrame
 
 
-def binary_stats(y_true, y_pred, title, y_encoder):
-    pos_label = "attacker"
+def binary_stats(y_true, y_pred, title, y_encoder, pos_label="attacker"):
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        y_true, y_pred, pos_label=pos_label, zero_division=0, average="binary"
+    )
     stats = DataFrame(
         {
             "accuracy": [accuracy_score(y_true, y_pred)],
-            "precision": [
-                precision_score(y_true, y_pred, pos_label=pos_label, zero_division=0)
-            ],
-            "recall": [
-                recall_score(y_true, y_pred, pos_label=pos_label, zero_division=0)
-            ],
-            "f1_score": [
-                f1_score(y_true, y_pred, pos_label=pos_label, zero_division=0)
-            ],
+            "precision": [precision],
+            "recall": [recall],
+            "f1_score": [f1],
             "confusion_matrix": [confusion_matrix(y_true, y_pred)],
         },
         index=[title],
@@ -43,5 +40,5 @@ def multiclass_stats(y_true, y_pred, title):
     return stats
 
 
-def multiclass_f1score_macro(y_true, y_pred):
-    return f1_score(y_true, y_pred, average="macro")
+def multiclass_f1score_weighted(y_true, y_pred):
+    return f1_score(y_true, y_pred, average="weighted")
