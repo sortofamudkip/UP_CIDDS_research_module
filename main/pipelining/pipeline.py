69dff6c3b39780a75a84a011217779534776bfac
Sortofamudkip <wishyutp0328@gmail.com>
2023-07-02 22:07:59 +0200

    pipeline for just 1 synth epoch eval

--- main/pipelining/pipeline.py
+++ main/pipelining/pipeline.py
@@ -12,13 +12,16 @@ from .pipeline_5classes import (
     eval_5classes,
     load_testdata_5classes,
 )
+from ..synthetic_eval.evaluate_synthetic import eval_synthetic_one_epoch
 
 
-def create_output_dir(pipeline_name: str) -> Path:
-    output_dir = Path(__file__).parent / "../../../results" / pipeline_name
+def create_output_dir(pipeline_name: str, skip_if_exists=False) -> Path:
+    output_dir = (Path(__file__).parent / "../../../results" / pipeline_name).resolve()
+    if skip_if_exists and output_dir.is_dir():
+        return output_dir
     try:
         output_dir.mkdir(parents=True, exist_ok=False)
-        print(f"Created output dir {output_dir}.")
+        print(f"Created output dir: {output_dir}")
     except FileExistsError:
         print(f"Dir already exists: {output_dir} ")
         assert False
@@ -52,6 +55,21 @@ def eval_dataset(
     return summary_df
 
 
+def generate_and_eval_dataset_once(
+    gan_pipeline,
+    X_test: np.array,
+    y_test: np.array,
+    num_classes: int,
+) -> pd.DataFrame:
+    assert num_classes in (2, 5), "Number of classes can only be 2 or 5"
+    num_rows_to_generate = gan_pipeline.X.shape[0]
+    generated_samples = gan_pipeline.generate_samples(num_rows_to_generate)
+    summary_df = eval_synthetic_one_epoch(
+        gan_pipeline, generated_samples, X_test, y_test, num_classes
+    )
+    return summary_df
+
+
 def run_pipeline(
     pipeline_name: str,  # ex: "N_2_25epochs_TSTR001"
     train_data_pickle_fname: str,  # ex: "./preprocessed/X_y_2_classes_N_train"
@@ -62,12 +80,17 @@ def run_pipeline(
     num_classes: int,  # ex: 2
     batch_size: int = 1024,
     learning_rate: float = 0.00001,
+    fold: str = "",  # used to give different file names for crossval
 ):
     # basic checks
     assert num_classes == 2 or num_classes == 5
 
-    output_dir = create_output_dir(pipeline_name)
-    with open(output_dir / "log.txt", "w") as f:
+    # make files with folds look nicer
+    if fold and not fold.startswith("_"):
+        fold = f"fold_{fold}"
+
+    output_dir = create_output_dir(pipeline_name, bool(fold))
+    with open(output_dir / f"log{fold}.txt", "w") as f:
         with redirect_stdout(f):
             # Load data & init pipline
             print("Loading training data and initialising GAN...")
@@ -84,6 +107,7 @@ def run_pipeline(
                 params = {
                     "pipeline_name": pipeline_name,
                     "train_data_pickle_fname": train_data_pickle_fname,
+                    "test_data_pickle_fname": test_data_pickle_fname,
                     "num_epochs": num_epochs,
                     "batch_size": batch_size,
                     "learning_rate": learning_rate,
@@ -107,21 +131,22 @@ def run_pipeline(
                     "d_loss": gan_history.history["d_loss"],
                 }
             ).rename_axis("Epoch")
-            losses.to_csv(f"results/{pipeline_name}/losses.csv")
+            losses.to_csv(f"results/{pipeline_name}/losses{fold}.csv")
             losses.plot(title="GAN Losses", ylabel="loss").get_figure().savefig(
-                f"results/{pipeline_name}/losses.jpg"
+                f"results/{pipeline_name}/losses{fold}.jpg"
             )
 
             # load test data
             print("Loading test data...")
             X_test, y_test = load_testdata(test_data_pickle_fname, num_classes)
 
-            # evaluate synthetic data
+            # generate and evaluate synthetic data
             print("Evaluating synthetic data...")
-            summary_df = eval_dataset(
-                pipeline_name, num_epochs, gan_pipeline, X_test, y_test, num_classes
+            summary_df = generate_and_eval_dataset_once(
+                gan_pipeline, X_test, y_test, num_classes
             )
-    return {
-        "summary_df": summary_df,
-        "gan": gan_pipeline,
-    }
+
+            # save summary data
+            with open(f"results/{pipeline_name}/metrics{fold}.json", "w") as f:
+                json.dump(summary_df, f, indent=4)
+    return {"summary_df": summary_df, "gan": gan_pipeline, "losses": losses}
--- main/synthetic_eval/evaluate_synthetic.py
+++ main/synthetic_eval/evaluate_synthetic.py
@@ -8,6 +8,58 @@ import UP_CIDDS_research_module.main.discrim_models.models as models
 import UP_CIDDS_research_module.main.score_model as scoring
 
 
+def eval_synthetic_one_epoch(
+    gan_pipeline: basic_gan.BasicGANPipeline,
+    synthetic_samples: np.array,
+    X_test: np.array,
+    y_test: np.array,
+    num_classes: int,
+):
+    print("Begin synthetic evaluation")
+    plausibility_score = eval_plaus_score(gan_pipeline, synthetic_samples)
+    # evaluate on linear and non-linear models
+    X_train, y_train, y_encoder = decode_samples_to_np(gan_pipeline, synthetic_samples)
+    if len(np.unique(y_train)) == 1:
+        print("y only has one unique label!")
+        tstr_forest_f1, tstr_logreg_f1 = 0, 0
+    else:
+        # train models and obtain predictions
+        y_predict_forest, y_predict_logreg = tstr_predict_forest_logreg(
+            X_test, y_test, X_train, y_train, y_encoder
+        )
+        # evaluate models
+        tstr_forest_f1 = scoring.f1_stats_one_epoch(
+            y_test, y_predict_forest, num_classes
+        )
+        tstr_logreg_f1 = scoring.f1_stats_one_epoch(
+            y_test, y_predict_logreg, num_classes
+        )
+        # gather results
+    summary_df = {
+        "plaus_score": plausibility_score,
+        "TSTR_forest_f1": tstr_forest_f1,
+        "TSTR_logreg_f1": tstr_logreg_f1,
+    }
+    return summary_df
+
+
+def tstr_predict_forest_logreg(X_test, y_test, X_train, y_train, y_encoder):
+    y_predict_forest = models.random_forest_train_predict(
+        X_train, X_test, y_train, y_test, y_encoder, n_estimators=20, n_jobs=4
+    )
+    y_predict_logreg = models.logistic_reg_train_predict(
+        X_train,
+        X_test,
+        y_train,
+        y_test,
+        y_encoder,
+        solver="newton-cg",
+        n_jobs=4,
+    )
+
+    return y_predict_forest, y_predict_logreg
+
+
 def eval_all_synthetic_2classes(
     gan_pipeline: basic_gan.BasicGANPipeline,
     X_test: np.array,
@@ -16,34 +68,27 @@ def eval_all_synthetic_2classes(
     num_epochs: int,
 ):
     plaus_scores = []
-    tstr_tree_f1s = []
-    tstr_perceptron_f1s = []
+    tstr_forest_f1s = []
+    tstr_logreg_f1s = []
     for epoch in range(1, num_epochs + 1):
         print(f"Epoch {epoch} evaluation:\n=========")
         # load created data
-        synthetic_samples = np.load(
-            f"results/{pipeline_name}/synthetic_epoch{epoch}.npy"
-        )
+        synthetic_fname = f"results/{pipeline_name}/synthetic_epoch{epoch}.npy"
+        synthetic_samples = load_synthetic_data(synthetic_fname)
         # evaluate plausibility score
-        decoded_samples = gan_pipeline.decode_samples_to_human_format(synthetic_samples)
-        plausibility_score = dataset_scorer.score_data_plausibility_single(
-            decoded_samples, num_classes=2
-        )
+        plausibility_score = eval_plaus_score(gan_pipeline, synthetic_samples)
         plaus_scores.append(plausibility_score)
-        # print(f"overall plausibility_score: {plausibility_score}")
         # evaluate on linear and non-linear models
-        X_train, y_train, y_encoder = gan_pipeline.decode_samples_to_model_format(
-            synthetic_samples
+        X_train, y_train, y_encoder = decode_samples_to_np(
+            gan_pipeline, synthetic_samples
         )
         if len(np.unique(y_train)) == 1:
             print("y only has one unique label!")
-            y_predict_forest = False
-            y_predict_logreg = False
             tstr_N_2classes_forest_report = scoring.mode_collapse_binary_stats(
-                "TSTR_N_2classes_forest"
+                "TSTR_2classes_forest"
             )
             tstr_N_5classes_logreg_report = scoring.mode_collapse_binary_stats(
-                "TSTR_N_2classes_logreg"
+                "TSTR_2classes_logreg"
             )
         else:
             y_predict_forest = models.random_forest_train_predict(
@@ -65,18 +110,37 @@ def eval_all_synthetic_2classes(
             tstr_N_5classes_logreg_report = scoring.binary_stats(
                 y_test, y_predict_logreg, "TSTR_N_2classes_logreg", y_encoder
             )
-        tstr_tree_f1s.append(tstr_N_2classes_forest_report["f1_score"][0])
-        tstr_perceptron_f1s.append(tstr_N_5classes_logreg_report["f1_score"][0])
+        tstr_forest_f1s.append(tstr_N_2classes_forest_report["f1_score"][0])
+        tstr_logreg_f1s.append(tstr_N_5classes_logreg_report["f1_score"][0])
 
     summary_df = pd.DataFrame(
         {
             "plaus_score": plaus_scores,
-            "TSTR_tree_f1": tstr_tree_f1s,
-            "TSTR_perceptron_f1": tstr_perceptron_f1s,
+            "TSTR_tree_f1": tstr_forest_f1s,
+            "TSTR_perceptron_f1": tstr_logreg_f1s,
         }
     )
     summary_df.index += 1
     return summary_df
+
+
+def decode_samples_to_np(gan_pipeline, synthetic_samples):
+    return gan_pipeline.decode_samples_to_model_format(synthetic_samples)
+
+
+def load_synthetic_data(synthetic_fname: str):
+    return np.load(synthetic_fname)
+
+
+def eval_plaus_score(
+    gan_pipeline: basic_gan.BasicGANPipeline, synthetic_samples: np.array
+):
+    decoded_samples = gan_pipeline.decode_samples_to_human_format(synthetic_samples)
+    plausibility_score = dataset_scorer.score_data_plausibility_single(
+        decoded_samples, num_classes=2
+    )
+
+    return plausibility_score
 
 
 def eval_all_synthetic_5classes(
