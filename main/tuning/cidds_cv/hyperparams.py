f30bade0a081727396660466ccab6c556b645d79
sortofamudkip <29839553+sortofamudkip@users.noreply.github.com>
2023-11-29 22:25:13 +0100

    return with default hps

--- main/tuning/cidds_cv/hyperparams.py
+++ main/tuning/cidds_cv/hyperparams.py
@@ -4,11 +4,11 @@ import keras_tuner as kt
 DEFAULT_HYPERPARAMS_TO_TUNE = {
     "discriminator": {
         "hidden_layer_width": 128,
-        "learning_rate": 0.0002,
+        "learning_rate": 1e-5,
     },
     "generator": {
         "hidden_layer_width": 128,
-        "learning_rate": 0.0002,
+        "learning_rate": 1e-5,
     },
     "num_epochs": 10,
     "batch_size": 128,
@@ -29,19 +29,20 @@ def recursive_dict_union(dict1, dict2):
 
 def get_hyperparams_to_tune(hp: kt.HyperParameters):
     hyperparams_to_tune = {
-        "discriminator": {
-            "hidden_layer_width": hp.Int(
-                "hidden_layer_width", min_value=128, max_value=128 * 3, step=128
-            ),
-            "learning_rate": hp.Choice("learning_rate", values=[1e-3, 1e-4, 1e-5]),
-        },
-        "generator": {
-            "hidden_layer_width": hp.Int(
-                "hidden_layer_width", min_value=128, max_value=128 * 3, step=128
-            ),
-            "learning_rate": hp.Choice("learning_rate", values=[1e-3, 1e-4, 1e-5]),
-        },
-        "num_epochs": hp.Int("num_epochs", min_value=10, max_value=20, step=5),
-        "batch_size": hp.Int("batch_size", min_value=128, max_value=256, step=64),
+        # "discriminator": {
+        #     "hidden_layer_width": hp.Int(
+        #         "hidden_layer_width", min_value=128, max_value=128 * 3, step=128
+        #     ),
+        #     "learning_rate": hp.Choice("learning_rate", values=[1e-3, 1e-4, 1e-5]),
+        # },
+        # "generator": {
+        #     "hidden_layer_width": hp.Int(
+        #         "hidden_layer_width", min_value=128, max_value=128 * 3, step=128
+        #     ),
+        #     "learning_rate": hp.Choice("learning_rate", values=[1e-3, 1e-4, 1e-5]),
+        # },
+        # "num_epochs": hp.Int("num_epochs", min_value=10, max_value=20, step=5),
+        # "batch_size": hp.Int("batch_size", min_value=128, max_value=256, step=64),
     }
-    return hyperparams_to_tune
+    all_hps = recursive_dict_union(DEFAULT_HYPERPARAMS_TO_TUNE, hyperparams_to_tune)
+    return all_hps
