3ece5120917b508fb802d1f338131964156b77d3
sortofamudkip <29839553+sortofamudkip@users.noreply.github.com>
2023-12-01 18:18:08 +0100

    specify more hyperparams

--- main/tuning/cidds_cv/hyperparams.py
+++ main/tuning/cidds_cv/hyperparams.py
@@ -5,13 +5,18 @@ DEFAULT_HYPERPARAMS_TO_TUNE = {
     "discriminator": {
         "hidden_layer_width": 128,
         "learning_rate": 1e-5,
+        "hidden_layer_depth": 2,
+        "beta_1": 0.5,
     },
     "generator": {
         "hidden_layer_width": 128,
         "learning_rate": 1e-5,
+        "hidden_layer_depth": 2,
+        "beta_1": 0.5,
     },
     "num_epochs": 10,
     "batch_size": 128,
+    "latent_dim": 100,
 }
 
 
@@ -29,20 +34,37 @@ def recursive_dict_union(dict1, dict2):
 
 def get_hyperparams_to_tune(hp: kt.HyperParameters):
     hyperparams_to_tune = {
-        # "discriminator": {
-        #     "hidden_layer_width": hp.Int(
-        #         "hidden_layer_width", min_value=128, max_value=128 * 3, step=128
-        #     ),
-        #     "learning_rate": hp.Choice("learning_rate", values=[1e-3, 1e-4, 1e-5]),
-        # },
-        # "generator": {
-        #     "hidden_layer_width": hp.Int(
-        #         "hidden_layer_width", min_value=128, max_value=128 * 3, step=128
-        #     ),
-        #     "learning_rate": hp.Choice("learning_rate", values=[1e-3, 1e-4, 1e-5]),
-        # },
-        # "num_epochs": hp.Int("num_epochs", min_value=10, max_value=20, step=5),
-        # "batch_size": hp.Int("batch_size", min_value=128, max_value=256, step=64),
+        "discriminator": {
+            "hidden_layer_width": hp.Int(
+                "hidden_layer_width", min_value=128, max_value=128 * 5, step=128
+            ),
+            "learning_rate": hp.Choice(
+                "learning_rate", values=[3e-4, 8e-4, 3e-5, 8e-5, 3e-6]
+            ),
+            "hidden_layer_depth": hp.Int(
+                "hidden_layer_depth", min_value=3, max_value=10, step=2
+            ),
+            "beta_1": hp.Choice("beta_1", values=[0.0, 0.5, 0.9]),
+        },
+        "generator": {
+            "hidden_layer_width": hp.Int(
+                "hidden_layer_width", min_value=128, max_value=128 * 3, step=128
+            ),
+            "learning_rate": hp.Choice(
+                "learning_rate", values=[1e-4, 5e-4, 1e-5, 5e-5, 1e-6]
+            ),
+            "hidden_layer_depth": hp.Int(
+                "hidden_layer_depth", min_value=3, max_value=10, step=2
+            ),
+            "beta_1": hp.Choice("beta_1", values=[0.0, 0.5, 0.9]),
+        },
+        "num_epochs": hp.Int(
+            "num_epochs", min_value=10, max_value=20, step=5
+        ),  # ! change when done debugging
+        "batch_size": hp.Choice(
+            "batch_size", values=[128, 256, 1024, 4096, 8192, 16384]
+        ),
+        "latent_dim": hp.Int("latent_dim", min_value=100, max_value=300, step=50),
     }
     all_hps = recursive_dict_union(DEFAULT_HYPERPARAMS_TO_TUNE, hyperparams_to_tune)
     return all_hps
